83c5e49d02f4ab6acebcb589d5e71a8ec808f2e2
suigang <244153653@qq.com>
2019-03-29 13:06:17 +0800

    完善

---
+++ src/common/header/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {HeaderWapper} from './style';
+
+class Header extends Component {
+  render() {
+    return (
+        <HeaderWapper>111</HeaderWapper>
+    );
+  }
+}
+
+export default Header;
---
+++ src/common/header/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+export const HeaderWapper = styled.div`
+  height: 58px;
+  width: 100%;
+  background:red;
+`;
