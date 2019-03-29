f88b3c9237f88a3e19f365e6e7480341b7334344
suigang <244153653@qq.com>
2019-03-29 13:49:20 +0800

    头部样式
    Signed-off-by: suigang <244153653@qq.com>

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-import {HeaderWapper} from './style';
+import {HeaderWapper,Logo,Nav,NavItem,NavSearch,Addition,Button} from './style';
 
 class Header extends Component {
   render() {
     return (
-        <HeaderWapper>111</HeaderWapper>
+        <HeaderWapper>
+            <Logo href='/' />
+            <Nav>
+                <NavItem className='left active'>首页</NavItem>
+                <NavItem className='left'>下载APP</NavItem>
+                <NavItem className='right'>搜索</NavItem>
+                <NavItem className='right'>Aa</NavItem>
+                <NavSearch></NavSearch>
+            </Nav>
+            <Addition>
+                <Button className='writting'>写文章</Button>
+                <Button className='reg'>注册</Button>
+            </Addition>
+        </HeaderWapper>
     );
   }
 }
